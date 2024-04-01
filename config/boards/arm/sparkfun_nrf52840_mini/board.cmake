1ccb0d70a45a8f9fda40927e5a8a160674d126a1
Aurel <Aurel>
2024-03-31 23:09:36 -0400

    redo microdox config based on main repo + sparkfun nrf52840 mini

--- config/boards/arm/sparkfun_nrf52840_mini/board.cmake
+++ config/boards/arm/sparkfun_nrf52840_mini/board.cmake
@@ -2,5 +2,6 @@
 # string(APPEND CMAKE_ASM_FLAGS " -DCONFIG_NFCT_PINS_AS_GPIOS")
 # string(APPEND CMAKE_C_FLAGS " -DCONFIG_NFCT_PINS_AS_GPIOS")
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
---
+++ config/boards/shields/microdox_mod/README.md
@@ -0,0 +1,8 @@
+# Microdox
+
+Microdox is a 36 key split keyboard by Boardsource.
+
+Two variants are defined for this shield – V1 and V2. The layout is exactly the same between the
+two. Per [help documentation](https://www.boardsource.xyz/help/6129be4a9c85c6050be190d2), if you
+purchased your PCB before April 2022, use `microdox_mod_left`/`microdox_mod_right`. Otherwise, use
+`microdox_mod_v2_left`/`microdox_mod_v2_right`.
